6880f68cbaa56f343395bebf74320bda9749062f
scrapy@qq.com <1960958490@qq.com>
2019-09-16 09:52:22 +0800

    RSA加密
    宁波学院JS破解

---
+++ 17-宁波学院JS破解RSA/key.py
@@ -0,0 +1,25 @@
+import execjs
+
+
+def get_js_function(js_path, func_name, func_args):
+    '''
+    获取指定目录下的js代码, 并且指定js代码中函数的名字以及函数的参数。
+    :param js_path: js代码的位置
+    :param func_name: js代码中函数的名字
+    :param func_args: js代码中函数的参数
+    :return: 返回调用js函数的结果
+    '''
+
+    with open(js_path, encoding='utf-8') as fp:
+        js = fp.read()
+        ctx = execjs.compile(js)
+        return ctx.call(func_name, func_args)
+
+if __name__ == '__main__':
+    # 给个star吧
+    passwd = get_js_function('key.js', 'key', '')
+    print('*'*80)
+    print(passwd)
+    print('*'*80)
+    print('@欢迎Star！')
+    print('@有问题请联系: scrapy@qq.com')
